2f479a30a394df39feb3c2d5aa5c44f3bad41c16
shivani1668 <shivani.20244151@mnnit.ac.in>
2026-06-28 13:40:08 +0530

    Add files via upload

--- basic1/client/src/store/useChatStore.js
+++ basic1/client/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
 
 const useChatStore = create((set, get) => ({
   conversations: [],
